9bc51270efb17b7915fd3a292390f1e8427e9ad4
Ashok Kumar <ashokruhela@gmail.com>
2016-01-24 15:52:43 +0530

    shared renderer to modal from home

--- public/javascript/controllers/chartModalController.js
+++ public/javascript/controllers/chartModalController.js
@@ -1,6 +1,8 @@
 angular.module('vbiApp').controller('chartModalController', ['$scope', '$uibModalInstance', 'chartRenderer', 'parameters', function($scope, $uibModalInstance, chartRenderer, parameters) {
-	console.log(chartRenderer);
+	console.log(chartRenderer.render);
 	console.log($uibModalInstance);
+	$scope.chartRenderer = chartRenderer;
+	$scope.parameters = parameters;
 	$scope.hide = function () {
     $uibModalInstance.dismiss('cancel');
   	};
